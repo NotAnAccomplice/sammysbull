9d1564d7d9d09905649ac9bb67e29d00e25d69dc
Sammy Haugrud <smyhaug@gmail.com>
2019-04-15 21:30:22 -0400

    Initial commit

---
+++ src/main/java/com/sammy/sammysbull/SammysbullApplication.java
@@ -0,0 +1,13 @@
+package com.sammy.sammysbull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SammysbullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SammysbullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sammy/sammysbull/SammysbullApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sammy.sammysbull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SammysbullApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
